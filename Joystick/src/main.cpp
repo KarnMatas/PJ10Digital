05adbbc8073a56ab2544faad9131f3771e92c73a
Wachiraya <Wachiraya.nc@mail.kmutt.ac.th>
2019-11-25 14:45:21 +0700

    LM SW

--- Joystick/src/main.cpp
+++ Joystick/src/main.cpp
@@ -1,10 +1,20 @@
 #include <mbed.h>
-
+Serial pc(USBTX,USBRX,38400);
+Serial bt(PA_11,PA_12,38400);
+DigitalIn buttonA(D2);
+DigitalIn buttonB(D3);
+DigitalIn buttonC(D4);
+DigitalIn buttonD(D5);
+AnalogIn X(A0);
+AnalogIn Y(A1);
+uint8_t ch ;
+char  DirX,DirY;
 int main() {
-
-  // put your setup code here, to run once:
-
   while(1) {
-    // put your main code here, to run repeatedly:
+      //////////////////// MASTER Joystick //////////////////////////////////
+            pc.printf("%d %d %d %d  X =  %.1f   : Y =   %.1f \n",buttonA.read(),buttonD.read(),buttonC.read(),buttonB.read(),X.read(),Y.read());
+			bt.printf("%d %d %d %d  X =  %.1f   : Y =   %.1f \n",buttonA.read(),buttonD.read(),buttonC.read(),buttonB.read(),X.read(),Y.read());	
+            wait(0.1);
   }
+  ///////////////////////////////////////////////////////////////////////////
 }
